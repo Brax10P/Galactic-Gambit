93434d587bf65315388a5e4810421d4a1ee9599e
Felix McGowan <fmcgowan@nic.edu>
2026-04-21 14:52:48 -0700

    created the code for deck creation and shuffling inside the game.h and game.cpp

--- header/game.h
+++ header/game.h
@@ -14,8 +14,15 @@
 #include "welcome.h"
 #include "play.h"
 #include "results.h"
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+#include <algorithm>
+#include <chrono>
 #include <SFML/Graphics.hpp>
 #include <iostream>
+using std::vector;
 
 class Game
 {
@@ -36,6 +43,19 @@ private:
     Play mGame;
     Results mResults;
    
+};
+
+class Deck
+{
+public:
+    Deck();
+    void setupDeck();       // - set the deck up using cardSprite and cardValue
+    void loadcardSprites(); // - load card sprites from sprite sheet
+
+private:
+    vector<sf::RectangleShape> cardSprites; // - Used to hold the card sprites
+    vector<int> cardValue;  // - holds the card's value based on the card
+    sf::Texture cardSpriteSheet;
 };
 
 #endif
